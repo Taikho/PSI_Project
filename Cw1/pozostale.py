f2fe3f3544c86a42c4cee2755cf732b3544d004b
Patryk Reweda <patrykreweda@gmail.com>
2020-10-29 13:58:32 +0100

    zad7 done

--- Cw1/pozostale.py
+++ Cw1/pozostale.py
@@ -7,6 +7,10 @@ lista2.insert(2, lista.pop(5))
 lista2.insert(3, lista.pop(5))
 lista2.insert(4, lista.pop(5))
 
+lista3 = lista + lista2
+lista3.insert(0, 0)
+lista3.sort(reverse=True)
 
 print(lista)
 print(lista2)
+print(lista3)
